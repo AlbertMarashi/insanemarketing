9a90083985eb3a148f6287cb817770f29b2119be
DominusVilicus <albertmashy@outlook.com>
2020-02-11 17:38:37 +1030

    remove db

--- server/server.js
+++ server/server.js
@@ -7,14 +7,10 @@ import { createSecureServer } from 'http2'
 import { createServer } from 'http'
 import router from './routes/router.js'
 import ssl from './bootstrap/ssl.js'
-import { dbsetup } from './bootstrap/db.js'
-
 async function startServer () {
     let app = new Koa()
     let httpServer = createServer(app.callback())
     let http2Server = createSecureServer({allowHTTP1: true}, app.callback())
-    
-    await dbsetup()
 
     if(ENV.ssl.enabled) {
         app.use(await ssl(httpServer, http2Server)) //generate SSL certificate if one does not exist, or is expired
